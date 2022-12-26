d5e512a39e8b0796e26be99330f2096cf4e630b3
Robin <robinmeow97@gmail.com>
2022-12-26 18:30:01 +0100

    Aim is now world relative

--- Assets/CubeShooter/Controls/Aiming.cs
+++ Assets/CubeShooter/Controls/Aiming.cs
@@ -3,32 +3,27 @@ using UnityEngine.Assertions;
 
 public sealed class Aiming : MonoBehaviour
 {
-    [SerializeField] Transform _aimingTransform = null;
     [SerializeField] UserInputs _userInputs = null;
+    Vector2 _userAimDirection = Vector2.up;
 
     void Awake()
     {
-        Assert.IsNotNull(_aimingTransform, $"{nameof(_aimingTransform)} may not be null.");
         Assert.IsNotNull(_userInputs, $"{nameof(_userInputs)} may not be null.");
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        Aim();
+        if (_userInputs.AimDirection != Vector2.zero)
+            _userAimDirection = _userInputs.AimDirection;
+        ApplyAim(_userAimDirection);
     }
 
-    void Aim()
+    void ApplyAim(Vector2 aimDirection)
     {
-        Vector2 aimDirection = _userInputs.AimDirection;
-        bool isAiming = aimDirection != Vector2.zero;
-        if (isAiming)
-        {
-            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-            if (angle < 0)
-                angle += 360; // apperently executes when aiming on Y negativ values 
-            _aimingTransform.Rotate(new Vector3(0.0f, 0.0f, angle), Space.World);
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        if (angle < 0)
+            angle += 360; // apperently executes when aiming on Y negativ values 
 
-            _aimingTransform.localEulerAngles = new Vector3(0.0f, 0.0f, angle - 90.0f);
-        }
+        transform.eulerAngles = new Vector3(0.0f, 0.0f, angle - 90.0f);
     }
 }
