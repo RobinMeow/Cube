464d7f4025e61a66a44ca02dfca9e29f204bf9c7
Robin <robinmeow97@gmail.com>
2022-12-28 21:21:53 +0100

    extracted too much responsibility out of JumpCalculator
    into Movement.cs

--- Assets/CubeShooter/Controls/JumpCalculator.cs
+++ Assets/CubeShooter/Controls/JumpCalculator.cs
@@ -7,83 +7,57 @@ using UnityEngine.Assertions;
 
 public sealed class JumpCalculator 
 {
-    readonly BaseInputs _inputs = null;
     readonly JumpStats _stats = null;
-    readonly Rigidbody _rigidbody;
-    readonly TextMeshPro _holdJumpPercentage;
     readonly GameTimer _holdingTime = null;
     static GameLogger _loggerInstance = new GameLogger("JumpCalculator");
     static GameLogger _logger => _loggerInstance;
-    bool _isJumping = false;
-
-    public bool IsJumping { get => _isJumping; }
+    bool _isCharging = false;
+    public bool IsCharging { get => _isCharging; }
+    
     //public bool JumpStarted { get; private set; }
 
-    public JumpCalculator(BaseInputs inputs, JumpStats stats, Rigidbody rigidbody, TextMeshPro holdJumpPercentage)
+    public JumpCalculator(JumpStats stats)
     {
-        Assert.IsNotNull(inputs, $"{nameof(JumpCalculator)} requires {nameof(inputs)} typeof {nameof(UserInputs)}");
-        Assert.IsNotNull(stats, $"{nameof(JumpCalculator)} requires {nameof(stats)} typeof {nameof(JumpStats)}");
-        Assert.IsNotNull(rigidbody, $"{nameof(JumpCalculator)} requires {nameof(rigidbody)} typeof {nameof(Rigidbody)}");
+        Assert.IsNotNull(stats, $"{nameof(stats)} may not be null");
 
-        _inputs = inputs;
         _stats = stats;
-        _rigidbody = rigidbody;
-        _holdJumpPercentage = holdJumpPercentage;
-        _holdingTime = new GameTimer(_stats.MaxAccumulationDurationInSeconds);
+        _holdingTime = new GameTimer(stats.MaxAccumulationDurationInSeconds);
     }
-
-    public bool IsHolding() => _inputs.JumpIsPressed && _inputs.JumpWasPressedPreviousFixedUpdate;
 
     /// <summary>
     /// this should be called each frame
     /// </summary>
-    public float Calculate()
+    public float Calculate(bool jumpIsPressed, bool jumpWasPressedPreviousFixedUpdate, out float percentageComplete)
     {
         float calculatedStrength = 0.0f;
-        
-        bool IsReleased() => !_inputs.JumpIsPressed && _inputs.JumpWasPressedPreviousFixedUpdate;
+        percentageComplete = 0.0f;
 
-        if (IsHolding() && !_isJumping) 
+        bool hasReleasedJumpButton() => !jumpIsPressed && jumpWasPressedPreviousFixedUpdate;
+        bool hasStartedPressingDownJumpButton() => jumpIsPressed && !jumpWasPressedPreviousFixedUpdate;
+        bool isHoldingDownJumpButton() => jumpIsPressed && jumpWasPressedPreviousFixedUpdate;
+
+        if (hasStartedPressingDownJumpButton() || isHoldingDownJumpButton()) 
         {
             _holdingTime.Tick(Time.deltaTime);
-            float percentageComplete = _holdingTime.GetCompletedFactor() * 100.0f;
-            _holdJumpPercentage.text = $"{percentageComplete:00} %";
-
-            DisableGravity();
+            percentageComplete = _holdingTime.GetCompletedFactor() * 100.0f;
         }
-        else if (IsReleased() && !_isJumping) 
+        else if (hasReleasedJumpButton() && !_isCharging) 
         {
             float additionalStrength = CalculateAdditionalStrength(_holdingTime.Current);
             calculatedStrength = _stats.InitialStrength + additionalStrength;
             _holdingTime.ResetTime();
-            _logger.Log($"{nameof(IsReleased)} calcedStrength: {calculatedStrength}");
-            _isJumping = true;
+            _logger.Log($"{nameof(hasReleasedJumpButton)} calcedStrength: {calculatedStrength}");
+            _isCharging = true;
         }
         else
         {
-            // leave default: Zero 
-            _isJumping = false;
-            EnableGravity();
-
-            _holdJumpPercentage.text = String.Empty;
+            _isCharging = false;
         }
 
         return calculatedStrength;
     }
 
-    void EnableGravity()
-    {
-        if (!_rigidbody.useGravity)
-            _rigidbody.useGravity = true;
-    }
-
-    void DisableGravity()
-    {
-        _rigidbody.useGravity = false;
-        _rigidbody.velocity = Vector3.zero;
-    }
-
-    private float CalculateAdditionalStrength(float maximum)
+    float CalculateAdditionalStrength(float maximum)
     {
         return (maximum / _stats.AccumulationStepInSeconds) * _stats.AccumulatingStrength;
     }
