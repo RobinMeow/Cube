d7cc8ceb097fbb367d53bd2fbad4b473e4ca378f
Robin <robinmeow97@gmail.com>
2022-12-28 21:52:54 +0100

    JumpStats are not auto-props
    makes them testable

--- Assets/CubeShooter/Controls/JumpStats.cs
+++ Assets/CubeShooter/Controls/JumpStats.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "NewJumpStats",menuName = "Movement/JumpStats")]
 public sealed class JumpStats : ScriptableObject
 {
-    [SerializeField] float _initialStrength = 400.0f;
+    [field: SerializeField]
+    public float InitialStrength { get; set; } = 8000.0f;
 
+    [field: SerializeField]
     [Tooltip("The amount of force which is applied additionaly to the initial force, per accumulation step.")]
-    [SerializeField] float _accumulatingStrength = 75.0f;
-    [SerializeField] float _accumulationStepInSeconds = 0.1f;
-    [SerializeField] float _maxAccumulationDurationInSeconds = 1.5f;
+    public float AccumulatingStrength { get; set; } = 12500.0f;
 
-    public float InitialStrength => _initialStrength;
-    public float AccumulatingStrength => _accumulatingStrength;
-    public float AccumulationStepInSeconds => _accumulationStepInSeconds;
-    public float MaxAccumulationDurationInSeconds => _maxAccumulationDurationInSeconds;
+    [field: SerializeField]
+    public float AccumulationStepInSeconds { get; set; } = 0.2f;
+
+    [field: SerializeField]
+    public float MaxAccumulationDurationInSeconds { get; set; } = 0.5f;
 }
